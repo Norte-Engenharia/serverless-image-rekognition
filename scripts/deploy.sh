f3d6c3ca70dc10ca046304639afd48208d69e2d8
Gabriel Alonso <gabriel.alonso.cabral@proton.me>
2025-04-18 19:45:58 -0300

    [IMPROVEMENT] - serverless-image-rekognition - master

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# Load environment variables from .env file
+if [ -f .env ]; then
+    export $(cat .env | xargs)
+fi
+
 rm -rf node_modules dist lambda.zip
 yarn install --production
 yarn build
 zip -r lambda.zip dist node_modules
-terraform apply
+
+aws s3 cp lambda.zip s3://$BUCKET_NAME/development/lambda.zip --region us-east-1
 rm -rf lambda.zip
 yarn
